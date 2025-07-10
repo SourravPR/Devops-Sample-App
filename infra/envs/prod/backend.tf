ad3a95d755647d7fb2edbb7b80cb07c69a363492
SooriyaNarayana <sooriya1202@gmail.com>
2025-07-10 15:09:28 +0530

    infra pipeline update

--- infra/envs/prod/backend.tf
+++ infra/envs/prod/backend.tf
@@ -1,5 +1,4 @@
 bucket       = "devops-app-terraform-state-prod"
 key          = "prod/terraform.tfstate"
 region       = "us-east-1"
-use_lockfile = true
 encrypt      = true
--- infra/envs/test/backend.tf
+++ infra/envs/test/backend.tf
@@ -1,5 +1,4 @@
 bucket       = "devops-app-terraform-state-test"
 key          = "test/terraform.tfstate"
 region       = "us-east-1"
-use_lockfile = true
 encrypt      = true
