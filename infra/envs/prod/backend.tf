35b323305b98b7f67af8ca1a9cfbe0d14ae272a2
SooriyaNarayana <sooriya1202@gmail.com>
2025-07-10 15:10:28 +0530

    Format Terraform backend configuration for consistency

--- infra/envs/prod/backend.tf
+++ infra/envs/prod/backend.tf
@@ -1,4 +1,4 @@
-bucket       = "devops-app-terraform-state-prod"
-key          = "prod/terraform.tfstate"
-region       = "us-east-1"
-encrypt      = true
+bucket  = "devops-app-terraform-state-prod"
+key     = "prod/terraform.tfstate"
+region  = "us-east-1"
+encrypt = true
--- infra/envs/test/backend.tf
+++ infra/envs/test/backend.tf
@@ -1,4 +1,4 @@
-bucket       = "devops-app-terraform-state-test"
-key          = "test/terraform.tfstate"
-region       = "us-east-1"
-encrypt      = true
+bucket  = "devops-app-terraform-state-test"
+key     = "test/terraform.tfstate"
+region  = "us-east-1"
+encrypt = true
